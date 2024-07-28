22e124b578f9a23fa81682a7cb72465b4644f747
write2shivamgithub <shivamshankaraacharya@gmail.com>
2024-07-28 22:38:56 +0530

    add exp 2 bow vs tfidf

---
+++ notebooks/dagshub_setup.py
@@ -0,0 +1,13 @@
+import mlflow
+import dagshub
+
+mlflow.set_tracking_uri("https://dagshub.com/write2shivamgithub/mlops-mini-project.mlflow")
+
+import dagshub
+dagshub.init(repo_owner='write2shivamgithub', repo_name='mlops-mini-project', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
+
