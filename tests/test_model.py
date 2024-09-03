6cc127369eaddaee29bf93499296c3eb4d8633ef
write2shivamgithub <shivamshankaraacharya@gmail.com>
2024-09-03 10:28:07 +0530

    fixing performance test bug

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,16 +1,17 @@
-# model test v1 - loading the model
-import pandas as pd
+# load test + signature test + performance test
+
 import unittest
 import mlflow
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import os
+import pandas as pd
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import pickle
 
 class TestModelLoading(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        # setup dagshub credentials for mlflow tracking
+        # Set up DagsHub credentials for MLflow tracking
         dagshub_token = os.getenv("DAGSHUB_PAT")
         if not dagshub_token:
             raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
@@ -25,27 +26,26 @@ class TestModelLoading(unittest.TestCase):
         # Set up MLflow tracking URI
         mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
-
-
-        # Load the model from MLflow model registry
-        cls.model_name = "my_model"
-        cls.model_version = cls.get_latest_model_version(cls.model_name)
-        cls.model_uri = f'models:/{cls.model_name}/{cls.model_version}'
-        cls.model = mlflow.pyfunc.load_model(cls.model_uri)
+        # Load the new model from MLflow model registry
+        cls.new_model_name = "my_model"
+        cls.new_model_version = cls.get_latest_model_version(cls.new_model_name)
+        cls.new_model_uri = f'models:/{cls.new_model_name}/{cls.new_model_version}'
+        cls.new_model = mlflow.pyfunc.load_model(cls.new_model_uri)
 
         # Load the vectorizer
         cls.vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
 
+        # Load holdout test data
+        cls.holdout_data = pd.read_csv('data/processed/test_bow.csv')
 
     @staticmethod
-    def get_latest_model_version(model_name):
-        client = mlflow.MLflowClient()
-        latest_version = client.get_latest_versions(model_name, stages=["Staging"])
+    def get_latest_model_version(model_name, stage="Staging"):
+        client = mlflow.MlflowClient()
+        latest_version = client.get_latest_versions(model_name, stages=[stage])
         return latest_version[0].version if latest_version else None
-        
-    def test_model_loaded_proper1y(self):
-        self.assertIsNotNone(self.model)
 
+    def test_model_loaded_properly(self):
+        self.assertIsNotNone(self.new_model)
 
     def test_model_signature(self):
         # Create a dummy input for the model based on expected input shape
@@ -91,10 +91,3 @@ class TestModelLoading(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
-if __name__ == "__main__":
-    unittest.main()
-
-
-
-
